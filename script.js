acd9844066cde6e0ffe39fb72deb2fff37dd2fdf
ariandto <ariandto@gmail.com>
2024-01-07 11:05:59 +0700

    sc2

--- script.js
+++ script.js
@@ -12,17 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
 function process() {
   var text = document.getElementById('text').value;
   var password = document.getElementById('password').value;
-  var operation = document.querySelector('input[name="operation"]:checked');
+  var operationEncrypt = document.getElementById('encryptRadio').checked;
+  var operationDecrypt = document.getElementById('decryptRadio').checked;
 
-  if (text === '' || password === || operation === null) {
+  if (text === '' || password === '' || (!operationEncrypt && !operationDecrypt)) {
     alert('Data, password, and operation selection are required.');
     return;
   }
 
-  if (operation.value === 'encrypt') {
+  if (operationEncrypt) {
     var encrypted = CryptoJS.AES.encrypt(text, password);
     document.getElementById('result').innerHTML = encrypted;
-  } else if (operation.value === 'decrypt') {
+  } else if (operationDecrypt) {
     var encryptedValue = document.getElementById('EncryptedValue').innerHTML;
     if (encryptedValue === '') {
       alert('Encrypted data is required for decryption.');
